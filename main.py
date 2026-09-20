8b46d9d7da8b548303811b4eddca440ceae0bca3
emmanuel10-PG <emmanuelpaul679@gmail.com>
2026-09-20 21:51:27 +0000

    test de programme de avec affichage de messae simple avec python ( execution : python .\main.py )

---
+++ main.py
@@ -0,0 +1,19 @@
+# This is a sample Python script.
+
+# Press Maj+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import this
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    a = 1
+    ## print_hi('test')
+    print(print_hi('test'),a )
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
